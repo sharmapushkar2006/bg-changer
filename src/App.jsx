63e82834374da6c87028fc3284d2fac710ddca7d
Pushkar Sharma <spushkar2205@gmail.com>
2026-08-02 00:39:10 +0530

    Fixed bug which was caused due to missing css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+import "./App.css"
 function App() {
   let [color, setColor] = useState("aqua")
 
   return (
-    <div className='w-full h-screen' style={{backgroundColor:color}}>
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'> 
           <button className='bg-red-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('red')}>Red</button>
