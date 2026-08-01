e907e03f068293e7cd6ab89d1b4f4fbfa9cde932
Pushkar Sharma <spushkar2205@gmail.com>
2026-08-01 23:48:00 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [color, setColor] = useState("aqua")
 
   return (
-    <>
-    
-    </>
+    <div className='w-full h-screen' style={{backgroundColor:color}}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'> 
+          <button className='bg-red-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('red')}>Red</button>
+          <button className='bg-orange-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('orange')}>Orange</button>
+          <button className='bg-pink-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('pink')}>Pink</button>
+          <button className='bg-green-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('green')}>Green</button>
+          <button className='bg-blue-300 px-2 py-2 rounded-xl cursor-pointer'onClick={()=>setColor('blue')}>Blue</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
